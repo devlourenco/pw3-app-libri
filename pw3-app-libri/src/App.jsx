bd6b96f7f06b2c2d967c7e5a41f55fa0821bad9d
devlourenco <guilherme.gsldev@gmail.com>
2024-08-01 22:21:44 -0300

    Limpeza dos arquivos CSS e criação do primeiro componente

--- pw3-app-libri/src/App.jsx
+++ pw3-app-libri/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import CardBooks from "./components/CardBooks";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CardBooks
+        titulo="Os irmãos Karamázov"
+        autor="Fiódor Dostoiévski e Paulo Bezerra"
+      />
+
+      <CardBooks
+        titulo="Crime e castigo"
+        autor="Fiódor Dostoiévski e Paulo Bezerra"
+      />
+
+      <CardBooks
+        titulo="Noites brancas"
+        autor="Nivaldo dos Santos e Fiódor Dostoiévski"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ pw3-app-libri/src/components/CardBooks.jsx
@@ -0,0 +1,10 @@
+const CardBooks = ({ titulo, autor }) => {
+  return (
+    <div>
+      <h2>{titulo}</h2>
+      <p>{autor}</p>
+    </div>
+  );
+};
+
+export default CardBooks;
