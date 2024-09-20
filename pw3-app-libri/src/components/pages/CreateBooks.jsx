1a6e74b70cbffc754991d9f1010c7c7a147f0d15
devlourenco <guilherme.gsldev@gmail.com>
2024-09-19 22:09:27 -0300

    Implementação da listagem das categorias

--- pw3-app-libri/src/components/pages/CreateBooks.jsx
+++ pw3-app-libri/src/components/pages/CreateBooks.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import style from "./CreateBooks.module.css";
 import Input from "../forms/Input";
 import Select from "../forms/Select";
 import Button from "../forms/Button";
 const CreateBooks = () => {
+  
+  
+  useEffect(()=>{
+    fetch('http://localhost:5000/listagemCateorias', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+    })
+      .then((resp) => {
+        console.log("RESPOSTA:" + resp);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <section className={style.create_book_container}>
       <h1>Cadastrar Livro</h1>
